588c36136efc1c61fa3d8347a7d0fed4d7c2a84a
Žiga Stopinšek <sigi.kajzer@gmail.com>
2013-10-17 16:24:46 +0200

    Slovenian lang support

---
+++ dev/lang/hgjs.lang.sl.js
@@ -0,0 +1,16 @@
+HackerGame.loadLanguage({
+	"Toggle navigation": "Skrij/prikaži navigacijo",
+	"HackerGameJS": "HackerGameJS",
+	"Getting started": "Kako začeti?",
+	"Game": "Igra",
+	"Help": "Pomoč",
+	"Terminal": "Terminal",
+	"Editor": "Urejevalnik",
+	"Assignment": "Misija",
+	"Tasks": "Naloge",
+	"Try it out!": "Poskusi sam",
+	"Tasks completed": "Opravljene naloge",
+	"Time left": "Preostali čas",
+	"Assignments completed": "Opravljene misije",
+	"Disclaimer": "Omejitev odgovornosti"
+});
